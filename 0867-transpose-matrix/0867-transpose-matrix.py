c0e26e844092a59ae467d6a26e605f9672ca4cef
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-06-20 23:20:56 +0300

    Time: 0 ms (100%), Space: 18.8 MB (5.9%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,6 +1,15 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        transpose = [list(row) for row in zip(*matrix)]
+        rows = len(matrix)
+        cols = len(matrix[0])
+        transpose = []
+        for c in range(cols):
+            new_row = []
+            for r in range(rows):
+                new_row.append(matrix[r][c])
+            transpose.append(new_row)
+
         return transpose
+
 
         
